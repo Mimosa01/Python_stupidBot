fbf05572626a890fffa8560730fafd0c8089e0b2
Mimosa01 <65584012+Mimosa01@users.noreply.github.com>
2020-06-03 02:48:26 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-import config
+
 import telebot
 
-bot = telebot.TeleBot(config.token)
+bot = telebot.TeleBot("1298531617:AAE3SCepRRJSuEOHIclidw3MYZC0N54WNt8")
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
